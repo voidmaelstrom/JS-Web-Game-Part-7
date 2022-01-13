e2be1ef582131b1d51c21720599cc464772368d8
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-01-12 18:39:04 -0700

    Initial Commits

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
+const run = true;
+
 // have the NPC start walking east immediately
-npc.walkEast()
+async function moveNPC(){
+  await npc.walkNorth(1400)
+  await npc.walkEast(1200)
+  await npc.walkSouth(300)
+  await npc.walkEast(1500)
+  await npc.walkSouth(1500)
+  await npc.walkWest(2700)
+  await npc.walkNorth(400)
+  // await npc.walkEast(2000)
+  // npc.walkSouth()
+}
+
+moveNPC()
 
 // Create the inventory
 const inventory = newInventory()
